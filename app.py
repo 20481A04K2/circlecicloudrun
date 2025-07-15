14c81253a6293c6e79953155c0fba3b723ec9943
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-07-15 16:02:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return "✅ Hello from Flask on GCP VM via Docker & CircleCI!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
